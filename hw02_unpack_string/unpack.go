7e81d773781e1900c86aa6494ae6f9205fb1fc98
Your Name <you@example.com>
2022-06-15 00:44:28 +0300

    refactor

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -13,9 +13,13 @@ func Unpack(s string) (string, error) {
 	var builder strings.Builder
 	switch {
 	case len(s) > 0 && !unicode.IsDigit(rune(s[0])):
-		for _, char := range s {
-			if unicode.IsDigit(char) {
-				repeat, _ := strconv.Atoi(string(char))
+		chars := []rune(s)
+		for i := 0; i < len(chars); i++ {
+			if unicode.IsDigit(chars[i]) {
+				if unicode.IsDigit(chars[i+1]) {
+					return "", ErrInvalidString
+				}
+				repeat, _ := strconv.Atoi(string(chars[i]))
 				str := builder.String()
 				if repeat == 0 {
 					builder.Reset()
@@ -24,9 +28,23 @@ func Unpack(s string) (string, error) {
 					builder.WriteString(strings.Repeat(str[len(str)-1:], repeat-1))
 				}
 			} else {
-				builder.WriteString(string(char))
+				builder.WriteString(string(chars[i]))
 			}
 		}
+		// for idx, char := range s {
+		// 	if unicode.IsDigit(char) {
+		// 		repeat, _ := strconv.Atoi(string(char))
+		// 		str := builder.String()
+		// 		if repeat == 0 {
+		// 			builder.Reset()
+		// 			builder.WriteString(str[:len(str)-1])
+		// 		} else {
+		// 			builder.WriteString(strings.Repeat(str[len(str)-1:], repeat-1))
+		// 		}
+		// 	} else {
+		// 		builder.WriteString(string(char))
+		// 	}
+		// }
 		return builder.String(), nil
 	case len(s) > 0 && unicode.IsDigit(rune(s[0])):
 		return "", ErrInvalidString
