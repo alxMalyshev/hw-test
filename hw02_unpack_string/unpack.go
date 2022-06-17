f4e54a0b9bd34ee7c4c5e5da5bc242224c2c95ba
Aleksandr Malyshev <a.malyshev@sigma-it.ru>
2022-06-17 12:53:39 +0300

    improvement - add case for asterisk task

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -11,40 +11,42 @@ var ErrInvalidString = errors.New("invalid string")
 
 func Unpack(s string) (string, error) {
 	var builder strings.Builder
+	var lastState string
 	switch {
 	case len(s) > 0 && !unicode.IsDigit(rune(s[0])):
-		chars := []rune(s)
-		for i := 0; i < len(chars); i++ {
-			if unicode.IsDigit(chars[i]) {
-				if unicode.IsDigit(chars[i+1]) {
+		for _, char := range s {
+			switch {
+			case unicode.IsDigit(char):
+				str := builder.String()
+				if lastState == "char" {
+					repeat, _ := strconv.Atoi(string(char))
+					if repeat == 0 {
+						builder.Reset()
+						builder.WriteString(str[:len(str)-1])
+					} else {
+						builder.WriteString(strings.Repeat(str[len(str)-1:], repeat-1))
+					}
+				} else if lastState == "backslash" {
+					builder.WriteString(string(char))
+					lastState = "char"
+					continue
+				} else {
 					return "", ErrInvalidString
 				}
-				repeat, _ := strconv.Atoi(string(chars[i]))
-				str := builder.String()
-				if repeat == 0 {
-					builder.Reset()
-					builder.WriteString(str[:len(str)-1])
+				lastState = "num"
+			case string(char) == `\`:
+				if lastState == "backslash" {
+					builder.WriteString(string(char))
+					lastState = "char"
 				} else {
-					builder.WriteString(strings.Repeat(str[len(str)-1:], repeat-1))
+					lastState = "backslash"
+					continue
 				}
-			} else {
-				builder.WriteString(string(chars[i]))
+			default:
+				builder.WriteString(string(char))
+				lastState = "char"
 			}
 		}
-		// for idx, char := range s {
-		// 	if unicode.IsDigit(char) {
-		// 		repeat, _ := strconv.Atoi(string(char))
-		// 		str := builder.String()
-		// 		if repeat == 0 {
-		// 			builder.Reset()
-		// 			builder.WriteString(str[:len(str)-1])
-		// 		} else {
-		// 			builder.WriteString(strings.Repeat(str[len(str)-1:], repeat-1))
-		// 		}
-		// 	} else {
-		// 		builder.WriteString(string(char))
-		// 	}
-		// }
 		return builder.String(), nil
 	case len(s) > 0 && unicode.IsDigit(rune(s[0])):
 		return "", ErrInvalidString
