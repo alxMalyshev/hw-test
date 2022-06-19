c57ea6bb16ccadcd2df892f306e298dc179059c4
Your Name <you@example.com>
2022-06-19 23:24:15 +0300

    fix utf8 latters

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -12,43 +12,44 @@ var ErrInvalidString = errors.New("invalid string")
 func Unpack(s string) (string, error) {
 	var builder strings.Builder
 	var lastState string
+	val := []rune(s)
 	switch {
-	case len(s) > 0 && !unicode.IsDigit(rune(s[0])):
-		for _, char := range s {
+	case len(val) > 0 && !unicode.IsDigit(val[0]):
+		for i := 0; i <= len(val)-1; i++ {
 			switch {
-			case unicode.IsDigit(char):
-				str := builder.String()
+			case unicode.IsDigit(val[i]):
 				if lastState == "char" {
-					repeat, _ := strconv.Atoi(string(char))
+					repeat, _ := strconv.Atoi(string(val[i]))
 					if repeat == 0 {
+						str := []rune(builder.String())
 						builder.Reset()
-						builder.WriteString(str[:len(str)-1])
+						builder.WriteString(string(str[:len(str)-1]))
 					} else {
-						builder.WriteString(strings.Repeat(str[len(str)-1:], repeat-1))
+						builder.WriteString(strings.Repeat(string(val[i-1]), repeat-1))
 					}
 				} else if lastState == "backslash" {
-					builder.WriteString(string(char))
+					builder.WriteString(string(val[i]))
 					lastState = "char"
 					continue
 				} else {
 					return "", ErrInvalidString
 				}
 				lastState = "num"
-			case string(char) == `\`:
+			case string(val[i]) == `\`:
 				if lastState == "backslash" {
-					builder.WriteString(string(char))
+					builder.WriteString(string(val[i]))
 					lastState = "char"
 				} else {
 					lastState = "backslash"
 					continue
 				}
 			default:
-				builder.WriteString(string(char))
+				builder.WriteString(string(val[i]))
 				lastState = "char"
 			}
 		}
 		return builder.String(), nil
-	case len(s) > 0 && unicode.IsDigit(rune(s[0])):
+	case len(val) > 0 && unicode.IsDigit(val[0]):
 		return "", ErrInvalidString
 	default:
 		return "", nil
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -21,6 +21,7 @@ func TestUnpack(t *testing.T) {
 		{input: `qwe\45`, expected: `qwe44444`},
 		{input: `qwe\\5`, expected: `qwe\\\\\`},
 		{input: `qwe\\\3`, expected: `qwe\3`},
+		{input: `Приве5т0 Мир6`, expected: `Привеееее Мирррррр`},
 	}
 
 	for _, tc := range tests {
