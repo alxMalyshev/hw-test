bb2d07c83c86735e70db51bbe747b41560990dd6
Your Name <you@example.com>
2022-07-07 23:52:33 +0300

    try to realize

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -17,10 +17,40 @@ type ListItem struct {
 }
 
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	// List ListItem
+	len int
+	front *ListItem
+	back *ListItem
 }
 
 func NewList() List {
 	return new(list)
 }
+
+func (l *list) Len() int { return l.len }
+
+func (l *list) Front() *ListItem {
+	
+	return
+}
+func (l *list) Back() *ListItem {return}
+
+func (l *list) PushFront(v interface{}) *ListItem {
+	newItem := &ListItem{
+		Value: v,
+	}
+	if l.front == nil {
+		l.front = newItem
+		l.back = newItem
+	} else {
+		newItem.Next = l.front
+		l.front.Prev = newItem
+		l.front = newItem
+	}
+	l.len ++
+	return l.front
+}
+
+func (l *list) PushBack(v interface{}) *ListItem {}
+func (l *list) Remove(i *ListItem) {}
+func (l *list) MoveToFront(i *ListItem) {}
