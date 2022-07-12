cc9dd0e2880e8a8c39f66768b5a6387959cab87b
Your Name <you@example.com>
2022-07-13 00:09:39 +0300

    cache functionality in progress

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -1,5 +1,9 @@
 package hw04lrucache
 
+import (
+	"github.com/alxMalyshev/hw-test/hw04_lru_cache/list"
+)
+
 type Key string
 
 type Cache interface {
@@ -9,11 +13,10 @@ type Cache interface {
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
-
+	// Cache // Remove me after realization.
 	capacity int
-	queue    List
-	items    map[Key]*ListItem
+	queue    list.List
+	items    map[Key]*list.ListItem
 }
 
 type cacheItem struct {
@@ -21,10 +24,25 @@ type cacheItem struct {
 	value interface{}
 }
 
-func NewCache(capacity int) Cache {
+func NewCache(capacity int) *lruCache {
 	return &lruCache{
 		capacity: capacity,
-		queue:    NewList(),
-		items:    make(map[Key]*ListItem, capacity),
+		queue:    list.NewList(),
+		items:    make(map[Key]*list.ListItem, capacity),
 	}
+
 }
+
+func (l *lruCache) Set(key Key, value interface{}) bool {
+	if item, ok := l.items[key]; ok {
+		l.queue.MoveToFront(value)
+		item.Value = value
+		return true
+	} else {
+		
+	}
+
+	return true
+}
+func (l *lruCache) Get(key Key) bool {return}
+func (l *lruCache) Clear() {return}
