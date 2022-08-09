e87f00bbfcae5ef2cd1f7db6c26164ae67b1f0b0
Your Name <you@example.com>
2022-08-09 21:38:26 +0300

    add some simple test

--- hw05_parallel_execution/run_test.go
+++ hw05_parallel_execution/run_test.go
@@ -15,26 +15,48 @@ import (
 func TestRun(t *testing.T) {
 	defer goleak.VerifyNone(t)
 
-	// t.Run("Simple test", func(t *testing.T){
-	// 	tasksCount := 50
-	// 	tasks := make([]Task, 0, tasksCount)
-	// 	var runTasksCount int32
-	// 	workersCount := 5
-	// 	maxErrorsCount := 5
+	t.Run("simple test", func(t *testing.T) {
+		tasksCount := 5
+		tasks := make([]Task, 0, tasksCount)
+		var runTasksCount int32
+		workersCount := 10
+		maxErrorsCount := 10
 
-	// 	for i := 0; i < tasksCount; i++ {toptop
-	// 		err := fmt.Errorf("error from task %d", i)
-	// 		tasks = append(tasks, func() error {
-	// 			time.Sleep(time.Millisecond * time.Duration(rand.Intn(100)))
-	// 			atomic.AddInt32(&runTasksCount, 1)
-	// 			return err
-	// 		})
-	// 	}
+		for i := 0; i < tasksCount; i++ {
+			err := fmt.Errorf("error from task %d", i)
+			tasks = append(tasks, func() error {
+				atomic.AddInt32(&runTasksCount, 1)
+				return err
+			})
+		}
 
-	// 	err := Run(tasks, workersCount, maxErrorsCount)
-	// 	fmt.Println(err)
+		err := Run(tasks, workersCount, maxErrorsCount)
 
-	// })
+		require.Equal(t, err, nil)
+	})
+
+	t.Run("invalid count of workers", func(t *testing.T) {
+		err := Run(nil, 0, 0)
+		require.Truef(t, errors.Is(err, ErrInvalidWorkerCount), "err is: %v", err)
+	})
+
+	t.Run("unlimit errors", func(t *testing.T) {
+		tasksCount := 50
+		tasks := make([]Task, 0, tasksCount)
+
+		var runTasksCount int32
+
+		for i := 0; i < tasksCount; i++ {
+			err := fmt.Errorf("error from task %d", i)
+			tasks = append(tasks, func() error {
+				atomic.AddInt32(&runTasksCount, 1)
+				return err
+			})
+		}
+
+		workersCount := 10
+		require.NoError(t, Run(tasks, workersCount, 0))
+	})
 
 	t.Run("if were errors in first M tasks, than finished not more N+M tasks", func(t *testing.T) {
 		tasksCount := 50
